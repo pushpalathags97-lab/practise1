5d1157f80a4384a026db67b4eb7719aabc6d23f2
pushpalathags97-lab <pushpalathags97@gmail.com>
2026-09-01 16:03:52 +0530

    Add files via upload

---
+++ conference.java
@@ -0,0 +1,12 @@
+public class conference {
+    public static void main(String[] args) {
+        int times = 5;
+        if(times > 10) {
+            System.out.println("Conference is ongoing.");
+        } else {
+            System.out.println("Conference has ended.");
+        }
+    }
+}
+    
+
